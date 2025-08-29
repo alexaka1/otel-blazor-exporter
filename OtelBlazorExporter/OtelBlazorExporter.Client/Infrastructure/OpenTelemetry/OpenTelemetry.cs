8761a05dd6a9cac7a680aa95041bd73eb33f59db
Alex Martossy <github@mail.martossy.hu>
2025-08-29 15:28:11 +0200

    Fix logger

--- OtelBlazorExporter/OtelBlazorExporter.Client/Infrastructure/OpenTelemetry/OpenTelemetry.cs
+++ OtelBlazorExporter/OtelBlazorExporter.Client/Infrastructure/OpenTelemetry/OpenTelemetry.cs
@@ -54,17 +54,21 @@ public static class OpenTelemetry
                 o.IncludeScopes = true;
                 o.ParseStateValues = true;
                 o.IncludeFormattedMessage = true;
-                var logExporterOptions = new OtlpExporterOptions
+
+                o.AddProcessor(provider =>
                 {
-                    Protocol = OtlpExportProtocol.HttpProtobuf,
-                    HttpClientFactory = () => new HttpClient(
-                        new JsInteropMessageHandler(
-                            null
-                        ), false) { BaseAddress = new Uri("http://localhost") },
-                    ExportProcessorType = ExportProcessorType.Simple,
-                };
-                var logExporter = new OtlpLogExporter(logExporterOptions);
-                o.AddProcessor(new SimpleLogRecordExportProcessor(logExporter));
+                    var logExporterOptions = new OtlpExporterOptions
+                    {
+                        Protocol = OtlpExportProtocol.HttpProtobuf,
+                        HttpClientFactory = () => new HttpClient(
+                            new JsInteropMessageHandler(
+                                provider.GetRequiredService<ILogger<JsInteropMessageHandler>>()
+                            ), false) { BaseAddress = new Uri("http://localhost") },
+                        ExportProcessorType = ExportProcessorType.Simple,
+                    };
+                    var logExporter = new OtlpLogExporter(logExporterOptions);
+                    return new SimpleLogRecordExportProcessor(logExporter);
+                });
                 // not having DI here REALLY sucks
                 o.SetResourceBuilder(ResourceBuilder.CreateDefault()
                     .AddService(Instrumentation.ClientServiceName, Instrumentation.Namespace)
