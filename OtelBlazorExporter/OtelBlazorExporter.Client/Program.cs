e27888b59a9cff123c8ffd2a93524c2ce774b9d0
Alex Martossy <github@mail.martossy.hu>
2025-08-29 12:08:31 +0200

    Add OpenTelemetry setup condition for WebAssembly and enhance logging in client app

--- OtelBlazorExporter/OtelBlazorExporter.Client/Program.cs
+++ OtelBlazorExporter/OtelBlazorExporter.Client/Program.cs
@@ -2,14 +2,27 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OtelBlazorExporter.Client.Infrastructure;
 using OtelBlazorExporter.Client.Infrastructure.OpenTelemetry;
 using System.Runtime.InteropServices.JavaScript;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Logging.ClearProviders();
-builder.Services.SetupOpenTelemetry();
+if (OperatingSystem.IsBrowser())
+{
+    builder.Services.SetupOpenTelemetry();
+}
+
 builder.AddLogging();
 if (OperatingSystem.IsBrowser())
 {
-    await JSHost.ImportAsync("OtelBlazorExporter.Client.OtlpExport", "/js/otlp-export.js");
+    await JSHost.ImportAsync("OtelBlazorExporter.Client.OtlpExport", "../js/otlp-export.js");
 }
 
-await builder.Build().RunAsync();
+var host = builder.Build();
+var logger = host.Services.GetRequiredService<ILoggerFactory>().CreateLogger($"{nameof(OtelBlazorExporter)}.{nameof(OtelBlazorExporter.Client)}");
+_ = host.Services.GetRequiredService<TracerProvider>();
+_ = host.Services.GetRequiredService<MeterProvider>();
+logger.LogInformation("Starting in WebAssembly mode");
+
+await host
+    .RunAsync();
--- OtelBlazorExporter/OtelBlazorExporter.Client/wwwroot/js/otlp-export.js
+++ OtelBlazorExporter/OtelBlazorExporter.Client/wwwroot/js/otlp-export.js
@@ -1,8 +1,11 @@
 export function sendExportRequest(bytes) {
   try {
     // bytes should arrive as a Uint8Array when imported via [JSImport]
+    console.log('sendExportRequest');
+    console.log(bytes);
     const u8 = bytes instanceof Uint8Array ? bytes : new Uint8Array(bytes);
     const blob = new Blob([u8], { type: 'application/x-protobuf' });
+    console.log(blob);
     navigator.sendBeacon('http://localhost:4318/v1/traces', blob);
   } catch (e) {
     console.error('sendExportRequest failed', e);
